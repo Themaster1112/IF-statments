6818835314ff704786dc69924fac7256aa9f788f
Pythonforbasics <114282587+Themaster1112@users.noreply.github.com>
2024-11-08 16:08:10 +0000

    Create IF_statements.py

---
+++ IF_statements.py
@@ -0,0 +1,4 @@
+#Ahh yes the wonderful IF statements you may like it... or hate it... but its the same thing all together. IF is to determine what for example has to be right to happen for specific. Here is an example it will use one of my projects.
+Order=input("Enter order number.")
+if Order == "09876":
+    print("Order coming up")
